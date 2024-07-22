6c62e470a21d0333c7369cb27b8db29c2863afa7
PalkeshPrajapati <palkeshprajapati88@gmail.com>
2024-07-22 13:00:10 +0530

    Optimize Home pagec code

--- app/page.js
+++ app/page.js
@@ -9,16 +9,14 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
-import { Separator } from "@/components/ui/separator"
-
-
+} from "@/components/ui/accordion";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
@@ -27,8 +25,12 @@ export default function Home() {
 
       <header className="flex flex-col md:items-start items-center container bg-background md:pt-16 pb-11">
         <div className="mt-24 md:mt-16 md:ml-10 flex items-center flex-col w-full md:w-1/2 text-center md:text-left">
-          <h1 className="md:text-4xl text-3xl font-extrabold"><span className="text-secondary">User-Centric Excellence</span>: Our App Development services Tackles Your Pain Points</h1>
-          <h2 className="md:text-lg">Experience a Seamless Digital Journey with <span className="text-primary">Desun</span> - Where Every Line of Code Resolves Your Challenges and <span className="text-secondary">Elevates Your App Experience</span> to Unparalleled Heights.</h2>
+          <h1 className="md:text-4xl text-3xl font-extrabold">
+            <span className="text-secondary">User-Centric Excellence</span>: Our App Development services Tackle Your Pain Points
+          </h1>
+          <h2 className="md:text-lg">
+            Experience a Seamless Digital Journey with <span className="text-primary">Desun</span> - Where Every Line of Code Resolves Your Challenges and <span className="text-secondary">Elevates Your App Experience</span> to Unparalleled Heights.
+          </h2>
         </div>
         <div className="md:ml-10 mt-10 flex flex-col md:w-1/2 w-full">
           <ContactForm />
@@ -39,17 +41,13 @@ export default function Home() {
         {/* Logobar and "Full development cycle" section */}
         <section className="bg-white px-0 md:px-10 py-10 container">
           <div className="py-5 border-y-2 mb-14 overflow-hidden">
-            <Carousel opts={{
-              align: "start",
-              loop: true,
-            }}>
+            <Carousel opts={{ align: "start", loop: true }}>
               <CarouselContent>
-                <CarouselItem className="md:basis-1/2 lg:basis-1/3"><Image alt="logo png's" src="/logobar.png" className="border-2 border-black mx-auto" width={150} height={150} /></CarouselItem>
-                <CarouselItem className="md:basis-1/2 lg:basis-1/3"><Image alt="logo png's" src="/logobar.png" className="border-2 border-black mx-auto" width={150} height={150} /></CarouselItem>
-                <CarouselItem className="md:basis-1/2 lg:basis-1/3"><Image alt="logo png's" src="/logobar.png" className="border-2 border-black mx-auto" width={150} height={150} /></CarouselItem>
-                <CarouselItem className="md:basis-1/2 lg:basis-1/3"><Image alt="logo png's" src="/logobar.png" className="border-2 border-black mx-auto" width={150} height={150} /></CarouselItem>
-                <CarouselItem className="md:basis-1/2 lg:basis-1/3"><Image alt="logo png's" src="/logobar.png" className="border-2 border-black mx-auto" width={150} height={150} /></CarouselItem>
-                <CarouselItem className="md:basis-1/2 lg:basis-1/3"><Image alt="logo png's" src="/logobar.png" className="border-2 border-black mx-auto" width={150} height={150} /></CarouselItem>
+                {Array(6).fill("").map((_, index) => (
+                  <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                    <Image alt="logo png's" src="./logobar.png" className="border-2 border-black mx-auto" width={150} height={150} />
+                  </CarouselItem>
+                ))}
               </CarouselContent>
               <CarouselPrevious className="hidden md:block" />
               <CarouselNext className="hidden md:block" />
@@ -65,83 +63,45 @@ export default function Home() {
             <div className="col-start-1">
               <div className="mb-4">
                 <h3 className="font-bold text-lg">Web</h3>
-                <h4>PHP / Javascript / Node.JS / Web Socket / Socket.io / Vue.js/ Nuxt / MySQL / Laravel / GO lang / django / Python</h4>
+                <p>PHP / Javascript / Node.JS / Web Socket / Socket.io / Vue.js / Nuxt / MySQL / Laravel / GO lang / Django / Python</p>
               </div>
               <div>
                 <h3 className="font-bold text-lg">Mobile</h3>
-                <h4>Swift / Kotlin / Alamofire / Firebase / CoreData / Room / Realm / Coroutine / RxJava / RxSwift / Unit Test / Navigation</h4>
+                <p>Swift / Kotlin / Alamofire / Firebase / CoreData / Room / Realm / Coroutine / RxJava / RxSwift / Unit Test / Navigation</p>
               </div>
             </div>
 
             <Separator className="block md:hidden" />
 
             <div className="md:col-start-2 md:row-start-1 md:row-span-2 flex flex-col gap-5">
-              <Link href="#" className="hover:underline underline-offset-4 decoration-primary">
-                <h4>iOS development <span className="text-primary">&gt;</span></h4>
-              </Link>
-              <Link href="#" className="hover:underline underline-offset-4 hover:decoration-primary">
-                <h4>Android development <span className="text-primary">&gt;</span></h4>
-              </Link>
-              <Link href="#" className="hover:underline underline-offset-4 hover:decoration-primary">
-                <h4>Web development <span className="text-primary">&gt;</span></h4>
-              </Link>
-              <Link href="#" className="hover:underline underline-offset-4 hover:decoration-primary">
-                <h4>UI/UX design <span className="text-primary">&gt;</span></h4>
-              </Link>
-              <Link href="#" className="hover:underline underline-offset-4 hover:decoration-primary">
-                <h4>Testing <span className="text-primary">&gt;</span></h4>
-              </Link>
-              <Link href="#" className="hover:underline underline-offset-4 hover:decoration-primary">
-                <h4>Launch <span className="text-primary">&gt;</span></h4>
-              </Link>
-              <Link href="#" className="hover:underline underline-offset-4 hover:decoration-primary">
-                <h4>IT consulting <span className="text-primary">&gt;</span></h4>
-              </Link>
+              {["iOS development", "Android development", "Web development", "UI/UX design", "Testing", "Launch", "IT consulting"].map((service, index) => (
+                <Link key={index} href="#" className="hover:underline underline-offset-4 decoration-primary">
+                  <h4>{service} <span className="text-primary">&gt;</span></h4>
+                </Link>
+              ))}
             </div>
           </div>
-
-          
         </section>
 
         {/* FAQ section */}
         <section id="faq" className="bg-background md:px-14 py-10 container">
           <div>
-            <div className="md:text-3xl text-2xl font-bold my-5">FAQ</div>
-            <Accordion type="single" collapsible className="flex gap-14 flex-wrap">
-                <AccordionItem value="item-1" className="w-full md:w-[46%]">
-                  <AccordionTrigger className="font-bold text-start">What is the cost of a mobile application?</AccordionTrigger>
+            <h2 className="md:text-3xl text-2xl font-bold my-5">FAQ</h2>
+            <Accordion type="single" collapsible className="flex gap-x-10 gap-y-5 md:gap-10 flex-wrap">
+              {[
+                { question: "What is the cost of a mobile application?", answer: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis autem neque sed, atque eaque excepturi omnis sint ducimus asperiores, molestias ab consectetur a earum quod soluta. Cupiditate doloremque dignissimos voluptas quidem laborum." },
+                { question: "Do you provide a guarantee for the mobile application?", answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ab, expedita accusantium est tenetur eos adipisci reiciendis praesentium iste, explicabo blanditiis totam vero provident animi nisi maxime sint aliquid amet quibusdam? Consectetur, deserunt harum iste odit illum distinctio quidem assumenda sint inventore, velit suscipit ipsum?" },
+                { question: "How long will development take?", answer: "Development terms directly depend on the requirements for the mobile application, the characteristics of the company, as well as the wishes of the customer.\n\nAverage development time from start to finished application:\nMedium projects up to 3 months.\nLarge projects about 4-6 months.\nTo get a more accurate estimate of the project completion time, it is necessary to determine the main task of the application, think over its logic and functionality." },
+                { question: "I will not tell my idea, do you guarantee confidentiality?", answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, maiores repellat laborum, culpa corporis atque, nobis obcaecati esse numquam dicta harum officia fuga autem iste." }
+              ].map((item, index) => (
+                <AccordionItem key={index} value={`item-${index + 1}`} className="w-full md:w-[46%]">
+                  <AccordionTrigger className="font-bold text-start">{item.question}</AccordionTrigger>
                   <AccordionContent>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis autem neque sed, atque eaque excepturi omnis sint ducimus asperiores, molestias ab consectetur a earum quod soluta. Cupiditate doloremque dignissimos voluptas quidem laborum.
+                    {item.answer}
                   </AccordionContent>
                 </AccordionItem>
-
-                <AccordionItem value="item-2" className="w-full md:w-[46%]">
-                  <AccordionTrigger className="font-bold text-start">Do you provide a guarantee for the mobile application?</AccordionTrigger>
-                  <AccordionContent>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ab, expedita accusantium est tenetur eos adipisci reiciendis praesentium iste, explicabo blanditiis totam vero provident animi nisi maxime sint aliquid amet quibusdam? Consectetur, deserunt harum iste odit illum distinctio quidem assumenda sint inventore, velit suscipit ipsum?
-                  </AccordionContent>
-                </AccordionItem>
-
-                <AccordionItem value="item-3" className="w-full md:w-[46%]">
-                  <AccordionTrigger className="font-bold text-start">How long will development take?</AccordionTrigger>
-                  <AccordionContent>
-                      Development terms directly depend on the requirements for the mobile application, the characteristics of the company, as well as the wishes of the customer.<br/>
-                      <br/>
-                      Average development time from start to finished application:<br/>
-                      Medium projects up to 3 months.<br/>
-                      Large projects about 4-6 months.<br/>
-                      To get a more accurate estimate of the project completion time, it is necessary to determine the main task of the application, think over its logic and functionality.<br/>
-                  </AccordionContent>
-                </AccordionItem>
-
-                <AccordionItem value="item-4" className="w-full md:w-[46%]">
-                  <AccordionTrigger className="font-bold text-start">I will not tell my idea, do you guarantee confidentiality?</AccordionTrigger>
-                  <AccordionContent>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, maiores repellat laborum, culpa corporis atque, nobis obcaecati esse numquam dicta harum officia fuga autem iste.
-                  </AccordionContent>
-                </AccordionItem>
+              ))}
             </Accordion>
-
           </div>
         </section>
       </main>
